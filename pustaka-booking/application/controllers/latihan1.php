57d38739346ba61d60d3da7fc7d2b31fdc8b9265
ekarafi <semetondewata07@gmail.com>
2023-09-25 17:01:44 +0700

    menambahkan file latihan1, mengubah defaul controller

---
+++ pustaka-booking/application/controllers/latihan1.php
@@ -0,0 +1,16 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Biodata Mahasiswa BSI</h1>";
+        echo "Nama : Rafi Eka Candra<br>";
+        echo "Nim : 19220615<br>";
+        echo "Kelas : 19.3A.24<br>";
+        echo "Umur : 19 Tahun<br>";
+        echo "Alamat : Cileungsi, Bogor<br>";
+        echo "No.Hp : 081386692760<br>";
+        echo "Hobby : Mendengarkan Musik<br>";
+    }
+}
+?>
