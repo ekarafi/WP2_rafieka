ec537d291b93fcb72793bcce161902c61a131298
ekarafi <semetondewata07@gmail.com>
2023-09-25 11:53:03 +0700

    menambahkan pustaka-booking ke github

---
+++ pustaka-booking/application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rafi Eka 
+            Saya tinggal di daerah Cileungsi
+            hobby yang saya suka adalah mendengarkan Musik";
+    }
+}
