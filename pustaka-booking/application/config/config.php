00eb8e06a023ea586bc04712a009b510f3c4d671
ekarafi <semetondewata07@gmail.com>
2023-10-03 11:20:41 +0700

    menambahkan .htaccess

--- pustaka-booking/application/config/config.php
+++ pustaka-booking/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'localhost/WP2_rafieka/pustaka-booking/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- pustaka-booking/application/controllers/latihan2.php
+++ pustaka-booking/application/controllers/latihan2.php
@@ -13,9 +13,9 @@ class Latihan2 extends CI_Controller
         
         $data['nilai1'] = $n1;
         $data['nilai2'] = $n2;
-        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+        $data['hasil'] = $this->Model_latihan2->jumlah($n1, $n2);
         
-        $this->load->view('view-latihan1', $data);
+        $this->load->view('view_latihan2', $data);
     }
 
 }
