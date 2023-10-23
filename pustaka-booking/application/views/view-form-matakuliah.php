a02fe88cd8ca5e26ca2d5c36bfc95f7e22a282b0
semetondewata07@gmail.com <semetondewata07@gmail.com>
2023-10-23 17:11:30 +0700

    menambahkan tugas form validasi pada codeigniter

--- pustaka-booking/application/views/view-form-matakuliah.php
+++ pustaka-booking/application/views/view-form-matakuliah.php
@@ -24,6 +24,7 @@ method="post">
                     <th>:</th>
                     <td>
                         <input type="text" name="kode" id="kode">
+                        <div><?= form_error('kode'); ?></div>
                     </td>
                 </tr>
                 <tr>
@@ -31,6 +32,7 @@ method="post">
                     <td>:</td> 
                     <td>
                         <input type="text" name="nama" id="nama">
+                        <div><?= form_error('nama'); ?></div>
                     </td>
                 </tr>
                 <tr>
